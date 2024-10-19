b7106f7c28c5466ef78255934fcbdbe3505de6e7
phamhungcuong1112005@gmail.com <phamhungcuong1112005@gmail.com>
2024-10-19 14:17:41 +0700

    HUNG CUONG SUA

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -22,7 +22,7 @@ public class TileManager {
     public TileManager(GamePanel gp){
         this.gp = gp;
 
-        tile = new Tile[10];
+        tile = new Tile[50];
         mapTileNum= new int[gp.maxWorldCol][gp.maxWorldRow];
 
         getTileImage();
